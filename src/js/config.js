5bc443ec874a1c9ace66da5a232b2d83e3af4fbf
sagaradesara <sagaradesara1420@gmail.com>
2022-11-26 16:29:12 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b75d6031-0957-4a14-9e3b-8382d8c70117';
+export const MODAL_CLOSE_SEC = 2.5;
